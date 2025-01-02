10ff20348e081033c7747f5b17d4ef042fa3bf46
AhmedPythonCoder <122676257+AhmedPythonCoder@users.noreply.github.com>
2025-01-02 18:59:36 +0100

    Update lesson6.py

--- lesson6.py
+++ lesson6.py
@@ -7,17 +7,27 @@ HEIGHT = 700
 #set the title of the screen
 TITLE = 'rectangle'
 
-r = 232
-g = 20
-b = 213
 
-
+w = 400
+h = 600
 def draw():
-    #Rect(x,y,width,height) 
-    #screen.draw.circle(pos,radius,(r,g,b)) 
-    #Rect((0, 8),(700, 800) )
-    rect =     Rect((WIDTH/2, HEIGHT/2),(400, 400) )
-    rect.center=(WIDTH/2, HEIGHT/2)
-    screen.draw.rect(rect, (r,g,b))
+        w = 80
+        h = 40
+        r = 255
+        g = 100
+        b = 255
+        for i in range(30):
+
+        #Rect(x,y,width,height) 
+        #screen.draw.circle(pos,radius,(r,g,b)) 
+        #Rect((0, 8),(700, 800) )
+            rect =     Rect((WIDTH/2, HEIGHT/2 ),(w, h) )
+            rect.center=(WIDTH/2, HEIGHT/2)
+            screen.draw.rect(rect, (r,g,b))
+            r = r-5
+            g = g+5
+            b = b-5
+            w = w + 20 
+            h = h + 10
 #draw()
 pgzrun.go()
